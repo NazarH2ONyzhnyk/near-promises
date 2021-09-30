3dd7c45adc7115bb4fdebaca2d388a2e21181d9b
Nazar Nyzhnyk <nazarh2onyzhnyk@gmail.com>
2021-09-30 03:01:50 +0300

    home page

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        gray: {
+          700: "#2D2C41",
+          800: "#313045",
+        },
+        blue: {
+          400: "#5DAAE2",
+          500: "#2A65FC",
+        },
+        orange: {
+          400: "#FF7A00",
+          500: "#DC2828",
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
